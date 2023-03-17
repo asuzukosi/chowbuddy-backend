ed4a4b5327df2b0d1ead2b9299fde524bff90d4b
Kosisochukwu Asuzu <kosisochukwuasuzu@Kosisochukwus-MacBook-Pro.local>
2023-03-17 15:44:33 +0000

    feat(fetch-restaurants-script): impelemented script to fetch restuarants and load into db

--- chowbuddy/apps/restaurant/models.py
+++ chowbuddy/apps/restaurant/models.py
@@ -1,16 +1,41 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 
-# Create your models here.
+class RestaurantManager(models.Manager):
+    def create(self,*args, **kwargs):
+        username = kwargs["name"].lower().replace(" ","")
+        if get_user_model().objects.filter(username=username).exists():
+            user = get_user_model().objects.get(username=username)
+            
+        elif get_user_model().objects.filter(email=kwargs["email"]).exists():
+            user = get_user_model().objects.get(email=kwargs["email"])
+        
+        else:
+            user = get_user_model().objects.create(username=username, email=kwargs["email"], is_active=True, password=username)
+            user.set_password(username)
+            user.save()
+
+        return super(RestaurantManager, self).create(user=user, *args, **kwargs)
 
 class Restaurant(models.Model):
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name="restaurant")    
-    name = models.CharField(max_length=100)
-    address = models.CharField(max_length=100)
-    phone = models.CharField(max_length=100)
+    name = models.CharField(max_length=1000)
+    email = models.EmailField(blank=True, null=True)
+    description = models.TextField()
+    address = models.CharField(max_length=1000)
+    phone = models.CharField(max_length=1000)
     image = models.ImageField(upload_to='restaurant_images')
     long = models.FloatField()
     lat = models.FloatField()
+    rating = models.FloatField()
+    ranking = models.CharField(max_length=1000)
+    price_level = models.CharField(max_length=100)
+    
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    objects = RestaurantManager()
+    
     
     def __str__(self):
         return self.name
--- chowbuddy/utils/data_gathering_engine.py
+++ chowbuddy/utils/data_gathering_engine.py
@@ -3,3 +3,67 @@
 #   - Engine for collecting data from the https://tasty.p.rapidapi.com/ api.
 #   - Engine for collecting data from the https://worldwide-restaurants.p.rapidapi.com api.
 #   - Engine to load data collected from the API inot the restaurants, dishes and tags model
+import requests
+import shutil
+from django.core.files.uploadedfile import UploadedFile
+from restaurant.models import Restaurant
+
+def getRestaurantsFromWorldRestaurant():
+    url = "https://worldwide-restaurants.p.rapidapi.com/search"
+
+    payload = "language=en_US&limit=30&location_id=297704&currency=USD"
+    headers = {
+        "content-type": "application/x-www-form-urlencoded",
+        "X-RapidAPI-Key": "3556fae311msha4eee6b27ec2b1fp10839bjsnd8fca4b389aa",
+        "X-RapidAPI-Host": "worldwide-restaurants.p.rapidapi.com"
+    }
+
+    response = requests.request("POST", url, data=payload, headers=headers)
+    
+    data = response.json()["results"]["data"]
+    print("Gotten the data....")
+    for entry in data:
+        print("--------------------------------")
+        print(f"Getting the data for {entry['name']}...")
+        
+        name:str = entry["name"]
+        latitude = entry["latitude"]
+        longitude = entry["longitude"]
+        address = entry["address"]
+        ranking = entry["ranking"]
+        rating = entry["rating"]
+        price_level = entry["price_level"]
+        computed_email = name.lower().replace(" ", "")+"@gmail.com"
+        email = entry.get("email", computed_email)
+        phone = entry["phone"]
+        description = entry["description"]
+        if entry["photo"]["images"]["medium"]["url"]:
+            print("Getting the image...")
+            image_url = entry["photo"]["images"]["medium"]["url"]
+            res = requests.get(image_url, stream = True)
+            print("Image retrieved")
+            file_name = f"{name}.jpg"
+            if res.status_code == 200:
+                with open(file_name,'wb') as f:
+                    shutil.copyfileobj(res.raw, f)
+                    print('Image sucessfully Downloaded: ',file_name)
+            else:
+                print('Image Couldn\'t be retrieved')
+                
+        image = None
+        try:
+            image=UploadedFile(file=open(f"{name}.jpg", 'rb'))
+            print('Image Successfully Downloaded and loaded: ')
+        except Exception as e:
+            print("Image not found")
+        
+        if not Restaurant.objects.filter(name=name).exists():
+            print("Creating new restaurant ", name)
+            restaurant:Restaurant = Restaurant.objects.create(name=name, email=email, description=description, address=address, 
+                                                   phone=phone, image=image, long=longitude, 
+                                                   lat=latitude, rating=rating,
+                                                   ranking=ranking, price_level=price_level)
+            restaurant.save()
+            print("Created restaurant: ", name)
+            
+    return "complete"
